bdabdf4595eb0a5cad9c412c71558c365148e11e
EC2 Default User <ec2-user@ip-172-31-12-110.eu-central-1.compute.internal>
2020-12-28 21:47:30 +0000

    more excercices

--- RB101/small_problems/easy1.rb
+++ RB101/small_problems/easy1.rb
@@ -6,3 +6,34 @@ def repeat(string, num)
 end
 
 repeat('Hello',3)
+
+
+#Write a method that takes one integer argument, which may be positive, 
+#negative, or zero. This method returns true if the number's absolute value is 
+#odd. You may assume that the argument is a valid integer value.
+
+def is_odd?(num)
+  puts num % 2 != 0
+end
+  
+puts is_odd?(2)    # => false
+puts is_odd?(5)    # => true
+puts is_odd?(-17)  # => true
+puts is_odd?(-8)   # => false
+puts is_odd?(0)    # => false
+puts is_odd?(7)    # => true  
+
+#Write a method that takes one argument, a positive integer, 
+#and returns a list of the digits in the number.
+
+def digit_list(number)
+  number.to_s.split('').map {|x| x.to_i}
+end  
+
+  
+puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+puts digit_list(7) == [7]                     # => true
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+puts digit_list(444) == [4, 4, 4]             # => true
+    
+
