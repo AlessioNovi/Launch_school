3a55688f3f9d663c588ed1615af23e3feffa4299
EC2 Default User <ec2-user@ip-172-31-83-246.ec2.internal>
2021-01-01 20:56:52 +0000

    added excercise

--- RB101/small_problems/easy1.rb
+++ RB101/small_problems/easy1.rb
@@ -36,4 +36,29 @@ puts digit_list(7) == [7]                     # => true
 puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
 puts digit_list(444) == [4, 4, 4]             # => true
     
+#Write a method that counts the number of occurrences of each 
+#element in a given array.
 
+
+  vehicles = [
+  'car', 'car', 'truck', 'car', 'SUV', 'truck',
+  'motorcycle', 'motorcycle', 'car', 'truck'
+]
+
+def count_occurrences(array)
+
+single_occurrences = array.uniq
+occurences_array = []
+counter = 0
+
+while counter < single_occurrences.length
+  number_of_single_multiple_single_occurrences = 
+  array.count(single_occurrences[counter])
+  occurences_array << number_of_single_multiple_single_occurrences
+  puts "#{single_occurrences[counter]} => #{occurences_array[counter]} "
+  counter += 1
+end
+end
+
+
+count_occurrences(vehicles)
