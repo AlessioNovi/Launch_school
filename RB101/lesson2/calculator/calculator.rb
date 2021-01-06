b0a719368b90f44b09e6e9e2ad1aecae0c7e743f
EC2 Default User <ec2-user@ip-172-31-12-110.eu-central-1.compute.internal>
2021-01-06 16:19:28 +0000

    pushing calculator files for review

--- RB101/lesson2/calculator/calculator.rb
+++ RB101/lesson2/calculator/calculator.rb
@@ -7,6 +7,11 @@
 require 'yaml'
 
 MESSAGES = YAML.load_file('messages.yml')
+language = 'en'
+
+def messages(message, lang='en')
+  MESSAGES[message][lang]
+end
 
 def prompt(message)
   Kernel.puts("=> #{message}")
@@ -24,103 +29,171 @@ def number?(num)
   valid_number?(num) || valid_float?(num)
 end
 
-def operation_to_message(op)
-  outcome = case op
-            when '1'
-              MESSAGES['adding']
-            when '2'
-              MESSAGES['subtracting']
-            when '3'
-              MESSAGES['multiplying']
-            when '4'
-              MESSAGES['dividing']
-            end
-  outcome
+def operation_to_message(op, language)
+  case op
+  when '1'
+    messages(language, 'adding')
+  when '2'
+    messages(language, 'subtracting')
+  when '3'
+    messages(language, 'multiplying')
+  when '4'
+    messages(language, 'dividing')
+  end
+end
+
+def operator_case(operator, number1, number2)
+  case operator
+  when '1'
+    number1.to_f() + number2.to_f()
+  when '2'
+    number1.to_f() - number2.to_f()
+  when '3'
+    number1.to_f() * number2.to_f()
+  when '4'
+    number1.to_f() / number2.to_f()
+  end
+end
+
+def no_zero_div(op, num2)
+  %w(1 2 3 4).include?(op) && op == '4' && num2 == '0'
+end
+
+def operator_input_check(op)
+  %w(1 2 3 4).include?(op)
 end
 
 def play_again?(answ)
   answ == 'n' || answ == 'no'
 end
 
-def valid_answer?(an)
-  %w(y yes n no).include?(an)
+def valid_answer?(answ)
+  %w(y yes n no).include?(answ)
 end
 
-prompt(MESSAGES['welcome'])
+def clear
+  system 'clear'
+end
 
-name = ''
-loop do
-  name = Kernel.gets().chomp().strip
-  if name.empty?
-    prompt(MESSAGES['valid_name'])
-  else
-    break
+def restart
+  sleep 2
+  system 'clear'
+end
+
+def get_name(language)
+  name = ''
+  loop do
+    name = Kernel.gets().chomp().strip
+    if name.empty?
+      prompt(messages(language, 'valid_name'))
+    else
+      break
+    end
   end
+  name
 end
 
-prompt("#{MESSAGES['hello_name']} #{name}!")
-
-loop do # main loop
+def get_number_1(language)
   number1 = ''
   loop do
-    prompt(MESSAGES['first_number'])
+    prompt(messages(language, 'first_number'))
     number1 = Kernel.gets().chomp()
     if number?(number1)
       break
     else
-      prompt(MESSAGES['not_a_valid_number'])
+      prompt(messages(language, 'not_a_valid_number'))
     end
   end
+  number1
+end
+
+def get_number_2(language)
   number2 = ''
   loop do
-    prompt(MESSAGES['second_number'])
+    prompt(messages(language, 'second_number'))
     number2 = Kernel.gets().chomp()
     if number?(number2)
       break
     else
-      prompt(MESSAGES['not_a_valid_number'])
+      prompt(messages(language, 'not_a_valid_number'))
     end
   end
-  prompt(MESSAGES['operator_prompt'])
+  number2
+end
+
+def get_operator_and_check_zero_division(language, number2)
   operator = ''
   loop do
     operator = Kernel.gets().chomp()
-    if %w(1 2 3 4).include?(operator) && operator == '4' && number2 == '0'
-      prompt(MESSAGES['no_zero_division'])
-    elsif %w(1 2 3 4).include?(operator)
+    if no_zero_div(operator, number2)
+      prompt(messages(language, 'no_zero_division'))
+    elsif operator_input_check(operator)
       break
     else
-      prompt(MESSAGES['must_choose_1_2_3_or_4'])
+      prompt(messages(language, 'must_choose_1_2_3_or_4'))
     end
   end
-  prompt("#{operation_to_message(operator)} #{MESSAGES['the_two_numbers']}")
-  result = case operator
-           when '1'
-             number1.to_f() + number2.to_f()
-           when '2'
-             number1.to_f() - number2.to_f()
-           when '3'
-             number1.to_f() * number2.to_f()
-           when '4'
-             number1.to_f() / number2.to_f()
-           end
-  prompt("#{MESSAGES['the_result_is']} #{result}")
-  prompt(MESSAGES['another_calculation?'])
+  operator
+end
+
+def check_valid_answer(language)
   answer = ''
   loop do
     answer = Kernel.gets().chomp().downcase
     if valid_answer?(answer)
       break
-    else 
-      prompt(MESSAGES['wrong_input'])
+    else
+      prompt(messages(language, 'wrong_input'))
     end
   end
-  if play_again?(answer)
+  answer
+end
+
+def valid_language_choice?(language)
+  %w(en it).include?(language)
+end
+
+def set_language(language)
+  language_choice = ''
+  loop do
+    language_choice = Kernel.gets().chomp()
+    if valid_language_choice?(language_choice)
+      break
+    else
+      prompt(messages(language, 'valid_language'))
+    end
+  end
+  language_choice
+end
+
+clear # clears screen initially
+
+prompt(messages(language, 'welcome'))
+
+language = set_language(language) # asks for language
+
+prompt(messages(language, 'ask_name'))
+
+name = get_name(language) # asks for name
+
+prompt(messages(language, 'hello_name') + ' ' + name)
+
+loop do # main program loop
+  number1 = get_number_1(language)
+  number2 = get_number_2(language)
+  prompt(messages(language, 'operator_prompt'))
+  operator = get_operator_and_check_zero_division(language, number2)
+  prompt(operation_to_message(operator, language) + ' ' +
+  messages(language, 'the_two_numbers'))
+  result = operator_case(operator, number1, number2)
+  prompt(messages(language, 'the_result_is') + ' ' + result.to_s)
+  prompt(messages(language, 'another_calculation?'))
+  answer = check_valid_answer(language)
+  if play_again?(answer) # will exit loop if type n/no
     break
   else
-    prompt(MESSAGES['restart_calculator'])
-    sleep 2
-    system 'clear'
+    prompt(messages(language, 'restart_calculator'))
+    restart
   end
 end
-prompt(MESSAGES['thank_you'])
+prompt(messages(language, 'thank_you'))
